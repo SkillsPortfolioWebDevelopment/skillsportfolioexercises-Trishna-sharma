8a8fe5857482cfb4810095df32432a62c299c9f8
Trishna-sharma <91663993+Trishna-sharma@users.noreply.github.com>
2022-03-24 22:34:33 +0400

    C3:Styling those divs

---
+++ SetExercises-main/Chapter 3/Styling those divs/Styling those divs/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Styling_those_divs.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
