4f1833589d9be9e8b830bfb5054b19c1b6d44f78
Trishna-sharma <91663993+Trishna-sharma@users.noreply.github.com>
2022-04-22 15:27:56 +0400

    Create Alan Partridge soundboard.js

---
+++ SetExercises-main/Chapter 5/Alan Partridge soundboard.js
@@ -0,0 +1,51 @@
+function Audio1() {
+    document.getElementById("Ah Ha").play();
+    stopPlaying();
+};
+
+function Audio2() {
+    document.getElementById("Dan").play();
+    stopPlaying();
+};
+
+function Audio3() {
+    document.getElementById("Back of the net").play();
+    stopPlaying();
+};
+
+function  Audio4() {
+    document.getElementById("Bang out of order").play();
+    stopPlaying();
+};
+
+function Audio5() {
+    document.getElementById("Jurrassic park").play();
+    stopPlaying();
+};
+
+function Audio6() {
+    document.getElementById("smell my cheese").play();
+    stopPlaying();
+};
+
+function Audio7() {
+    document.getElementById("goal").play();
+    stopPlaying();
+};
+
+function Audio8() {
+    document.getElementById("kiss my face").play();
+    stopPlaying();
+};
+
+function Audio9() {
+    document.getElementById("hello partridge").play();
+    stopPlaying();
+};
+
+function stopPlaying() {
+    document.getElementById("lists");
+    audio.pause();
+    audio.currentTime = 0;
+}
+ 
